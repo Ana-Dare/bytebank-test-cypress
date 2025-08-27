baeeba7c560337c56aa3e0389d14f462326ffccd
Ana-Dare <analauradare467@gmail.com>
2025-08-27 11:26:08 -0300

    Testes customizados

---
+++ cypress/e2e/formulario.login.cy.js
@@ -0,0 +1,22 @@
+describe('Formulario de login', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+  it('Email válido', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('email-input').type('neilton@alura');
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O email digitado é inválido');
+  });
+  it.only('Não deve permitir campo em branco', () => {
+    cy.getByData('botao-login').click();
+    cy.getByData('senha-input').type('123456');
+    cy.getByData('botao-enviar').click();
+    cy.getByData('mensagem-erro')
+      .should('exist')
+      .and('have.text', 'O campo email é obrigatório');
+  });
+});
